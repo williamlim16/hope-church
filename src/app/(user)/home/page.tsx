a28a36e6754651ef663056455a521c489cb4c78a
williamlim16 <williaml1601@gmail.com>
2025-03-03 13:00:51 +1000

    feat[#8]: add middleware and logout

--- src/app/(user)/home/page.tsx
+++ src/app/(user)/home/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
 export default function Page() {
+  async function signOut() {
+    await authClient.signOut()
+  }
   return (
     <>
       Welcome!
+      <Button onClick={signOut}>Sign Out</Button>
     </>
   )
 }
---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request); // Optionally pass config as the second argument if cookie name or prefix is customized.
+  console.log("hellow")
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/home", "/admin/:path*"], // Specify the routes the middleware applies to
+};
