6c37ba964e11a589fc028acb2f5042477258d2e7
williamlim16 <williaml1601@gmail.com>
2025-03-08 10:55:41 +1000

    feat[#14]: created badge for event status

--- src/app/admin/event/columns.tsx
+++ src/app/admin/event/columns.tsx
@@ -1,26 +1,10 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { type SelectLifeGroup } from "@/db/schema"
+import { type SelectEvent } from "@/db/schema"
 import { type ColumnDef } from "@tanstack/react-table"
-import { MoreHorizontal, Pencil, Trash2 } from "lucide-react";
-import Link from "next/link";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input";
-import { deleteLifeGroup } from "@/actions/life-group-actions";
-import { useFormStatus } from "react-dom";
-import { useState } from "react";
+import { Badge } from "@/components/ui/badge";
 
-export const columns: ColumnDef<SelectLifeGroup>[] = [
+export const columns: ColumnDef<SelectEvent>[] = [
   {
     accessorKey: "name",
     header: "Name"
@@ -45,90 +29,21 @@ export const columns: ColumnDef<SelectLifeGroup>[] = [
     }
   },
   {
-    accessorKey: "updated_at",
-    header: "Updated At",
+    accessorKey: "status",
+    header: "Status",
     cell: ({ row }) => {
-      const updated = row.getValue("updated_at")
-      if (!updated) {
-        return
+      const event = row.original
+      if (event.status === "draft") {
+        return (
+          <Badge>Draft</Badge>
+        )
+      } else {
+        return (
+          <Badge>Published</Badge>
+        )
       }
-      const date = new Date(updated as Date)
-      return date.toLocaleString("en-US", { timeZone: "Australia/Sydney", day: "numeric", month: "long", year: "numeric" })
     }
   },
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      const lifeGroup = row.original
-
-
-      return (
-        <div className="flex gap-3">
-
-          <Link href={`/admin/life-group/${lifeGroup.id}`}>
-            <Button variant="ghost" size="sm">
-              <Pencil />
-            </Button>
-          </Link>
-
-          <DeleteDialog lifeGroup={lifeGroup} />
-
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem
-                onClick={() => navigator.clipboard.writeText(lifeGroup.voucher)}
-              >
-                Copy Voucher
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      )
-    }
-  }
 ]
 
-function DeleteButton() {
-  const { pending } = useFormStatus()
-  return (
-    <Button variant="destructive" disabled={pending}>
-      Delete
-    </Button>
-  )
-}
 
-function DeleteDialog({ lifeGroup }: { lifeGroup: SelectLifeGroup }) {
-
-  const [open, setOpen] = useState(false)
-
-  return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button variant="ghost" size="sm">
-          <Trash2 />
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete this life group.
-          </DialogDescription>
-          <DialogFooter >
-            <form action={deleteLifeGroup}>
-              <Input type="hidden" value={lifeGroup.id} name="lifeGroupId" />
-              <DeleteButton />
-            </form>
-          </DialogFooter>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  )
-}
--- src/app/admin/event/page.tsx
+++ src/app/admin/event/page.tsx
@@ -2,11 +2,15 @@
 import { Layout, LayoutHeader } from "@/components/layout/admin-layout"
 import { DataTable } from "@/components/ui/data-table"
 import { columns } from "./columns"
+import { eventList } from "@/server/services/event-service"
 export default async function Page() {
+
+  const event = await eventList()
 
   return (
     <Layout>
       <LayoutHeader addUrl="/admin/event/add">Event</LayoutHeader>
+      <DataTable data={event} columns={columns} />
     </Layout >
   )
 }
